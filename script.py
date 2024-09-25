d39ca7f9e4ac8394a21a23a81ca79ef3201b9efe
Murashu7 <murashu7@gmail.com>
2024-09-26 00:32:28 +0900

    拠点リストを読み込んで処理する#1

--- script.py
+++ script.py
@@ -136,7 +136,7 @@ async def trace_multiple_hosts(hosts_with_expected_routes, results_dir):
 
 async def process_trace(host, expected_route, results_dir):
     """個別のホストのtracerouteを処理"""
-    full_output_path = os.path.join(results_dir, f'{host}の結果.log')
+    full_output_path = os.path.join(results_dir, f'{host}への結果.log')
     t_current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     print(f"{host} の経路を確認中...")
@@ -157,12 +157,12 @@ async def process_trace(host, expected_route, results_dir):
     
     return {host: success}
 
-async def ping_trace_multiple_hosts(expected_ping_status, hosts_with_expected_routes):
+async def ping_trace_multiple_hosts(expected_ping_status, hosts_with_expected_routes, selected_name, selected_test_type):
     ping_results = {}
     trace_results = {}
     
-    # ファイルを一階層上の "results" フォルダに保存
-    results_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'results')
+    # ファイルを一階層上の "results/selected_name" フォルダに保存
+    results_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', f'results/{selected_name}/{selected_test_type}')
     
     # フォルダが存在しなければ作成
     os.makedirs(results_dir, exist_ok=True)
@@ -184,18 +184,125 @@ def convert_to_dict(df):
         
     return ping_list, trace_list
 
+def select_kyoten(df):
+    # 画面に全てのnumberとnameを表示
+    print("全てのnumberとnameを表示します:")
+    for index, row in df.iterrows():
+        print(f"拠点番号: {row['number']}, エリア: {row['area']}, 拠点名: {row['name']}")
+
+    # ユーザーが正しいnumberを選択するまでループ
+    selected_row = None
+    while selected_row is None or selected_row.empty:
+        try:
+            # numberを選択するメッセージを表示
+            selected_number = int(input("試験をする拠点番号を選択してください: "))
+            
+            # 選択されたnumberに対応する行を取得
+            selected_row = df[df['number'] == selected_number]
+            
+            if selected_row.empty:
+                print("選択したnumberは存在しません。もう一度選択してください。")
+        except ValueError:
+            print("無効な入力です。数字を入力してください。")
+
+    # 正しいnumberが選択されたらnameとtypeを変数に格納
+    selected_area = selected_row['area'].values[0]
+    selected_name = selected_row['name'].values[0]
+    selected_kyoten_type = selected_row['type'].values[0]
+    print(f"選択された拠点: {selected_name}({selected_kyoten_type})")
+    return selected_name, selected_kyoten_type
+
+def select_test_type(selected_kyoten_type):
+    # 画面に全ての試験内容を表示
+    print("試験内容を表示します:")
+    if selected_kyoten_type == '大規模':
+        numbers = [1, 2, 3, 4, 5]
+        types = ['正常性試験・復旧試験・L2SW試験', '帯域保証網試験', 'ベストエフォート網試験', 'ルータ1号機', 'ルータ2号機']
+        for index, row in enumerate(types):
+            print(f"番号: {index+1}, {row}")
+    elif selected_kyoten_type == '中規模':
+        numbers = [1, 2, 3]
+        types = ['正常性試験・復旧試験・L2SW試験', '帯域保証網試験', 'ベストエフォート網試験']
+        for index, row in enumerate(types):
+            print(f"番号: {index+1}, {row}")
+    elif selected_kyoten_type == '小規模':
+        numbers = [1, 2]
+        types = ['正常性試験・復旧試験・L2SW試験', 'ベストエフォート網試験']
+        for index, row in enumerate(types):
+            print(f"番号: {index+1}, {row}")
+    # TODO: その他拠点
+    else:
+        print("その他拠点は試験ができません。")
+        return
+    
+    data = {
+        'number': numbers,
+        'type': types
+    }
+    df = pd.DataFrame(data)
+    
+    # ユーザーが正しいnumberを選択するまでループ
+    selected_row = None
+    while selected_row is None or selected_row.empty:
+        try:
+            # numberを選択するメッセージを表示
+            selected_number = int(input("試験番号を選択してください: "))
+            
+            # 選択されたnumberに対応する行を取得
+            selected_row = df[df['number'] == selected_number]
+            
+            if selected_row.empty:
+                print("選択したnumberは存在しません。もう一度選択してください。")
+        except ValueError:
+            print("無効な入力です。数字を入力してください。")
+
+    # 正しいnumberが選択されたらnameとtypeを変数に格納
+    selected_test_type = selected_row['type'].values[0]
+    print(f"選択された試験: {selected_test_type}")
+    return selected_test_type
+
 # テスト実行
 def main():
     # CSVファイルの読み込み
-    csv_file = 'files/network_test_evaluation.csv'  # CSVファイルのパスを指定
-
+    csv_kyoten_list = '../files/kyoten_list.csv' 
+    df_kyoten = pd.read_csv(csv_kyoten_list)
+    selected_name, selected_kyoten_type = select_kyoten(df_kyoten)
+    selected_test_type = select_test_type(selected_kyoten_type)
+    
+    # 拠点のタイプから判定表を指定する
+    if selected_kyoten_type == "大規模":
+        if selected_test_type == "常性試験・復旧試験・L2SW試験":
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+        elif selected_test_type == "帯域保証網試験":
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+        elif selected_test_type == "ベストエフォート網試験":
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+        elif selected_test_type == "ルータ1号機":
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+        else:
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+    elif selected_kyoten_type == "中規模":
+        if selected_test_type == "常性試験・復旧試験・L2SW試験":
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+        elif selected_test_type == "帯域保証網試験":
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+        else:
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+    elif selected_kyoten_type == "小規模":
+        if selected_test_type == "常性試験・復旧試験・L2SW試験":
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+        else:
+            csv_net_test_eval = '../files/network_test_evaluation.csv'  # CSVファイルのパスを指定
+    # TODO: その他拠点
+    else:
+        return
+    
     # pandasでCSVを読み込む
-    df = pd.read_csv(csv_file)
+    df_csv_net_test_eval = pd.read_csv(csv_net_test_eval)
 
     # 複数のホストと期待されるpingの結果とtrace経路のリスト
-    expected_ping_status, hosts_with_expected_routes = convert_to_dict(df)
-    # ping_results, trace_results = ping_trace_multiple_hosts(expected_ping_status, hosts_with_expected_routes)
-    ping_results, trace_results = asyncio.run(ping_trace_multiple_hosts(expected_ping_status, hosts_with_expected_routes))
+    expected_ping_status, hosts_with_expected_routes = convert_to_dict(df_csv_net_test_eval)
+    ping_results, trace_results = asyncio.run(ping_trace_multiple_hosts(expected_ping_status, hosts_with_expected_routes, selected_name, selected_test_type))
 
     print(ping_results, trace_results)
 
